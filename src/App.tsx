227b707364d38ca2269270ccf66da2563473fe92
Simo Ngquseka <simo@swssr.co>
2021-07-01 01:38:32 +0200

    Finish base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 //#region Imports
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { AllCandidates } from './db';
 
-import logo from "./logo.svg"
+import logo from "./assets/logo.svg"
+import sortIcon from "./assets/sort.svg"
+import { Candidate } from './models';
+import { relativeTimeFromDates } from './helpers';
 //#endregion Imports
 
 function App() {
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
+
+  useEffect(() => {
+    setCandidates(AllCandidates);
+  }, [])
+
   return (
     <div className="app">
       <nav className="nav nav--top">
@@ -16,8 +26,57 @@ function App() {
       <main className="main">
         <div className="content-wrapper">
           <div className="subhead">
-            <div className="count">7 interview requests</div>
+            <span className="count">{candidates.length} interview requests</span>
           </div>
+          <table>
+            <thead>
+              <tr className="row row--head">
+                <th>
+                  <div className="head"><span>Candidate</span>
+                    <img src={sortIcon} alt="sort icon" /></div>
+                </th>
+                <th>
+                  <div className="head"><span>Role</span>
+                    <img src={sortIcon} alt="sort icon" /></div>
+                </th>
+                <th>
+                  <div className="head"><span>last Communication</span>
+                    <img src={sortIcon} alt="sort icon" /></div>
+                </th>
+                <th>
+                  <div className="head"><span>Salary</span>
+                    <img src={sortIcon} alt="sort icon" /></div>
+                </th>
+                <th>
+                  <div className="head"><span>Sent By</span>
+                    <img src={sortIcon} alt="sort icon" /></div>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {candidates.map(row => (
+                <tr key={row.candidate} className={"row" + (row.last_comms.unread ? " row--important" : "")}>
+                  <td>
+                    <div className="nested">
+                      <span><img src={row.image} alt={row.candidate} className="avator" /></span>
+                      <span>{row.candidate}</span>
+                    </div>
+                  </td>
+                  <td>{row.role}</td>
+                  <td>
+                    <div className="nested">
+                      <span className={"indicator" + (row.last_comms.unread ? " indicator--active" : "")}></span>
+                      <span>{row.last_comms.description}</span>
+                      <span className="minor">{relativeTimeFromDates(new Date(row.last_comms.date_time))}</span>
+                    </div>
+                  </td>
+                  <td>{row.salary}</td>
+                  <td>{row.sent_by}</td>
+                </tr>
+              ))}
+            </tbody>
+
+          </table>
         </div>
       </main>
     </div>
