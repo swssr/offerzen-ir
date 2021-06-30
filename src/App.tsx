33b3a571d3d7a46414920d2b8c9200c362ed1396
Simo Ngquseka <simo@swssr.co>
2021-06-30 23:14:33 +0200

    Add base container styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+//#region Imports
 import React from 'react';
 import './App.css';
 
+import logo from "./logo.svg"
+//#endregion Imports
+
 function App() {
   return (
-    <div className="main">
+    <div className="app">
       <nav className="nav nav--top">
-        <div className="content-wrapper">
-
+        <div className="content-wrapper content-wrapper--nav">
+          <img src={logo} alt="offerzen logo" />
         </div>
       </nav>
       <main className="main">
-
+        <div className="content-wrapper">
+          <div className="subhead">
+            <div className="count">7 interview requests</div>
+          </div>
+        </div>
       </main>
     </div>
   );
